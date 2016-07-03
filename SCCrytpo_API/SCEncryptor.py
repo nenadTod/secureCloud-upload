5c6049797c1d0f009f0436d961913e024201c072
nenadtod <nenadtod@live.com>
2016-07-03 16:08:46 +0200

    Odradjena izmena vezana za biranje foldera u koji se smestaju meta - porebno zbog web aplikacije.

--- SCCrytpo_API/SCEncryptor.py
+++ SCCrytpo_API/SCEncryptor.py
@@ -43,11 +43,17 @@ class SCEncryptor:
 
     def encryptLocal(self, file_list, drive, upload_location):
 
+        if not os.path.exists(self.temp_dir):
+            os.makedirs(self.temp_dir)
+
         user_id = drive.get_user_data()
 
-        file_pub = None
-        file_pri = None
-        folder_cloud = None
+        meta_pub = self.temp_dir + "/" + self.meta1E
+        meta_pri = self.temp_dir + "/" + self.meta1D
+
+        file_pub = None  # stored
+        file_pri = None  # stored
+        folder_cloud = None  # stored
 
         if isinstance(drive,  GoogleDriveAPI):
             file_pub = self.storage_folder + "/" + self.storage_GD_folder + "/" + user_id + "/" + self.storage_file_pub
@@ -72,17 +78,16 @@ class SCEncryptor:
                 pub_key = fhI.read()
                 key = RSA.importKey(pub_key)
 
-            file_id = drive.get_meta_file(upload_location, self.meta1DEnum)
+            file_id = drive.get_meta_file(upload_location, self.temp_dir, self.meta1DEnum)
 
-            if os.stat(self.meta1D).st_size == 0:
+            if os.stat(meta_pri).st_size == 0:
 
-                drive.get_meta_file("root", self.meta1DEnum)
-                if os.stat(self.meta1D).st_size == 0:
+                drive.get_meta_file("root", self.temp_dir, self.meta1DEnum)
+                if os.stat(meta_pri).st_size == 0:
                     # pop up - nesto nije bas po protokolu
                     return
 
-                drive.update_meta_file(file_id, self.meta1DEnum)
-                os.remove(self.meta1D)
+                drive.update_meta_file(file_id, self.temp_dir, self.meta1DEnum)
 
         else:
             sc = SCCrypto()
@@ -99,27 +104,33 @@ class SCEncryptor:
             with open(file_pri, 'w') as fhO:
                     fhO.write(xord[0])
 
-            file_id = drive.get_meta_file(upload_location, self.meta1DEnum)
-            with open(self.meta1D, 'w') as fhO:
+            file_id = drive.get_meta_file(upload_location, self.temp_dir, self.meta1DEnum)
+            with open(meta_pri, 'w') as fhO:
                 fhO.write(xord[1])
 
-            drive.update_meta_file(file_id, self.meta1DEnum)
-            os.remove(self.meta1D)
+            drive.update_meta_file(file_id, self.temp_dir, self.meta1DEnum)
+            os.remove(meta_pri)
 
-            file_id = drive.get_meta_file("root", self.meta1DEnum)
-            with open(self.meta1D, 'w') as fhO:
+            file_id = drive.get_meta_file("root", self.temp_dir, self.meta1DEnum)
+            with open(meta_pri, 'w') as fhO:
                 fhO.write(xord[1])
 
-            drive.update_meta_file(file_id, self.meta1DEnum)
-            os.remove(self.meta1D)
+            drive.update_meta_file(file_id, self.temp_dir, self.meta1DEnum)
 
-        self._do_the_job(self.meta1E, self.meta1EEnum, file_list, key, upload_location, drive)
+        self._do_the_job(meta_pub, self.meta1EEnum, file_list, key, upload_location, drive)
 
 
     def encryptShared(self, file_list, drive, upload_location):
 
-        id = drive.get_user_data()
-        hid = SHA256.new(id).hexdigest()
+        if not os.path.exists(self.temp_dir):
+            os.makedirs(self.temp_dir)
+
+        meta_pri = self.temp_dir + "/" + self.meta2D
+        meta_pub = self.temp_dir + "/" + self.meta2E
+
+
+        uid = drive.get_user_data()
+        hid = SHA256.new(uid).hexdigest()
 
         r = requests.post('http://127.0.0.1:8000/api/exist/', json={"id": hid})  # check if entity with that id exists
         dct = json.loads(r.content)
@@ -142,20 +153,20 @@ class SCEncryptor:
             prKPart = dct[0]['PrKPart']
 
             # save key part in a file located in the root folder
-            file_id = drive.get_meta_file("root", self.meta2DEnum)
-            with open(self.meta2D, 'w') as fhO:
+            file_id = drive.get_meta_file("root", self.temp_dir, self.meta2DEnum)
+            with open(meta_pri, 'w') as fhO:
                 fhO.write(prKPart)
 
-            drive.update_meta_file(file_id, self.meta2DEnum)
-            os.remove(self.meta2D)
+            drive.update_meta_file(file_id, self.temp_dir, self.meta2DEnum)
+            os.remove(meta_pri)
 
             # save key part in a file located in the upload folder
-            file_id = drive.get_meta_file(upload_location, self.meta2DEnum)
-            with open(self.meta2D, 'w') as fhO:
+            file_id = drive.get_meta_file(upload_location, self.temp_dir, self.meta2DEnum)
+            with open(meta_pri, 'w') as fhO:
                 fhO.write(prKPart)
 
-            drive.update_meta_file(file_id, self.meta2DEnum)
-            os.remove(self.meta2D)
+            drive.update_meta_file(file_id, self.temp_dir, self.meta2DEnum)
+            os.remove(meta_pri)
 
         else:
             psw = "hassan"#оve vrednosti ce se preuzimati sa forme.
@@ -176,26 +187,28 @@ class SCEncryptor:
             key = RSA.importKey(pub_key)
 
             # get a file with part of the private key located in the root folder
-            drive.get_meta_file("root", self.meta2DEnum)
-            with open(self.meta2D, 'r') as fhO:
+            drive.get_meta_file("root", self.temp_dir, self.meta2DEnum)
+            if os.stat(meta_pri).st_size == 0:
+                # pop up - nesto nije po protokolu, restart ?
+                return
+
+            with open(meta_pri, 'r') as fhO:
                 prKPart = fhO.read()
 
-            os.remove(self.meta2D)
+            os.remove(meta_pri)
 
             # save that part of the private key in a meta file located in the upload folder
-            file_id = drive.get_meta_file(upload_location, self.meta2DEnum)
-            with open(self.meta2D, 'w') as fhO:
-                fhO.write(prKPart)
+            file_id = drive.get_meta_file(upload_location, self.temp_dir, self.meta2DEnum)
 
-            drive.update_meta_file(file_id, self.meta2DEnum)
-            os.remove(self.meta2D)
+            if os.stat(meta_pri).st_size == 0:
+                with open(meta_pri, 'w') as fhO:
+                    fhO.write(prKPart)
 
-        self._do_the_job(self.meta2E, self.meta2EEnum, file_list, key, upload_location, drive)
+                drive.update_meta_file(file_id, self.meta2DEnum)
+
+        self._do_the_job(meta_pub, self.meta2EEnum, file_list, key, upload_location, drive)
 
     def _do_the_job(self, metaE, metaEEnum, file_list, key, upload_location, drive):
-
-        if not os.path.exists(self.temp_dir):
-            os.makedirs(self.temp_dir)
 
         sc = SCCrypto()
 
@@ -204,7 +217,7 @@ class SCEncryptor:
         enc_sym_key = ret_val[1]  # encrypted symmetric key
         iv_list = ret_val[2]  # iv for every encrypted file, in the same order
 
-        file_id = drive.get_meta_file(upload_location, metaEEnum)
+        file_id = drive.get_meta_file(upload_location, self.temp_dir, metaEEnum)
 
         with open(metaE, 'a') as fhO:
             fhO.write(enc_sym_key + "\n")
@@ -213,8 +226,7 @@ class SCEncryptor:
                 fhO.write(ntpath.split(f)[1] + " " + str(iv_list[i]) + "\n")
                 i += 1
 
-        drive.update_meta_file(file_id, metaEEnum)
-        os.remove(metaE)
+        drive.update_meta_file(file_id, self.temp_dir, metaEEnum)
 
         drive.upload(enc_file_list, upload_location)
 
