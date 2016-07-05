631906df3692dedc739958d44f957ca2b194dbce
vuletic <vuletic93@gmail.com>
2016-07-05 12:37:31 +0200

    cloud api update

--- cloud_API/abstract_drive_API.py
+++ cloud_API/abstract_drive_API.py
@@ -28,6 +28,22 @@ class AbstractDriveAPI(object):
     def download_files(self, folder_id, download_path):
         pass
 
-    #@abstractmethod
-    #def get_meta_files(self, folder_id):
-        #pass
+    @abstractmethod
+    def download_file(self, folder_id, file_name, download_path):
+        pass
+
+    @abstractmethod
+    def download_shared_file(self, folder_id, file_name, download_path):
+        pass
+
+    @abstractmethod
+    def get_user_id_by_folder_id(self, folder_id):
+        pass
+
+    @abstractmethod
+    def get_meta_file(self, folder_name, save_to_folder, meta_type):
+        pass
+
+    @abstractmethod
+    def update_meta_file(self, file_id, from_folder, meta_type):
+        pass
